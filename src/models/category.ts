53ab91a5167a52b066758ba387da5468e7267370
Noam Kurtzer <noamkurtzer@gmail.com>
2023-01-23 22:58:42 +0200

    Added English category names

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,8 +1,9 @@
-import {images} from '../constants/images';
+import { images } from "../constants/images";
 
 export interface Category {
   id: number;
   name: string;
+  englishName: string;
   isWideImage: boolean;
   image: any;
 }
@@ -10,55 +11,55 @@ export interface Category {
 export const CATEGORIES: Category[] = [
   {
     id: 1,
-    name: 'ראשונות',
+    name: "ראשונות",
     isWideImage: true,
     image: images.appetizerYellow,
   },
   {
     id: 2,
-    name: 'מרקים',
+    name: "מרקים",
     isWideImage: false,
     image: images.soupGreen,
   },
   {
     id: 3,
-    name: 'סלטים',
+    name: "סלטים",
     isWideImage: false,
     image: images.saladPink,
   },
   {
     id: 4,
-    name: 'עופות',
+    name: "עופות",
     isWideImage: false,
     image: images.chickenRed,
   },
   {
     id: 5,
-    name: 'בשרים',
+    name: "בשרים",
     isWideImage: true,
     image: images.steakBlue,
   },
   {
     id: 6,
-    name: 'דגים',
+    name: "דגים",
     isWideImage: false,
     image: images.salmonPink,
   },
   {
     id: 7,
-    name: 'פסטה',
+    name: "פסטה",
     isWideImage: false,
     image: images.pastaPink,
   },
   {
     id: 8,
-    name: 'קינוחים',
+    name: "קינוחים",
     isWideImage: false,
     image: images.cakeGreen,
   },
   {
     id: 9,
-    name: 'קוקטיילים',
+    name: "קוקטיילים",
     isWideImage: true,
     image: images.cocktailOrange,
   },
@@ -68,29 +69,68 @@ export const CategoryCards: Category[][] = [
   [
     {
       id: 1,
-      name: 'ראשונות',
+      name: "ראשונות",
+      englishName: "Starters",
       isWideImage: true,
       image: images.appetizerYellow,
     },
     {
       id: 2,
-      name: 'מרקים',
+      name: "מרקים",
+      englishName: "Soups",
       isWideImage: false,
       image: images.soupGreen,
     },
-    {id: 3, name: 'סלטים', isWideImage: false, image: images.saladPink},
+    {
+      id: 3,
+      name: "סלטים",
+      englishName: "Salads",
+      isWideImage: false,
+      image: images.saladPink,
+    },
   ],
   [
-    {id: 4, name: 'עופות', isWideImage: false, image: images.chickenRed},
-    {id: 5, name: 'בשרים', isWideImage: true, image: images.steakBlue},
-    {id: 6, name: 'דגים', isWideImage: false, image: images.salmonPink},
+    {
+      id: 4,
+      name: "עופות",
+      englishName: "Chicken",
+      isWideImage: false,
+      image: images.chickenRed,
+    },
+    {
+      id: 5,
+      name: "בשרים",
+      englishName: "Meat",
+      isWideImage: true,
+      image: images.steakBlue,
+    },
+    {
+      id: 6,
+      name: "דגים",
+      englishName: "Fish",
+      isWideImage: false,
+      image: images.salmonPink,
+    },
   ],
   [
-    {id: 7, name: 'פסטה', isWideImage: false, image: images.pastaPink},
-    {id: 8, name: 'קינוחים', isWideImage: false, image: images.cakeGreen},
+    {
+      id: 7,
+      name: "פסטה",
+      englishName: "Pasta",
+      isWideImage: false,
+      image: images.pastaPink,
+    },
+    {
+      id: 8,
+      name: "קינוחים",
+      englishName: "Desserts",
+      isWideImage: false,
+      image: images.cakeGreen,
+    },
     {
       id: 9,
-      name: 'קוקטיילים',
+      name: "קוקטיילים",
+      englishName: "Cocktails",
       isWideImage: true,
       image: images.cocktailOrange,
     },

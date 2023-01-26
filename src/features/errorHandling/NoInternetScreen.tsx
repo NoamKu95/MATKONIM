3815ac9b4005b99e5183a8adbb47d9f00330d679
Noam Kurtzer <noamkurtzer@gmail.com>
2023-01-26 10:15:10 +0200

    noInternetScreen design - texts & icon

--- src/features/errorHandling/NoInternetScreen.tsx
+++ src/features/errorHandling/NoInternetScreen.tsx
@@ -1,6 +1,12 @@
 // Outer imports:
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  GestureResponderEvent,
+  Image,
+  StyleSheet,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import i18n from "../../translations/i18n";
 
@@ -10,38 +16,50 @@ import MediumText from "../../components/text/MediumText";
 import RegularText from "../../components/text/RegularText";
 import { paddings } from "../../constants/paddings";
 import { pop } from "../../navigation/RootNavigation";
+import { icons } from "../../constants/icons";
+import ActionButton from "../../components/Buttons/ActionButton";
+import BoldText from "../../components/text/BoldText";
 
 const NoInternetScreen = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.flex}>
-        {/* <SomeRelevantIcon /> */}
+        <Image
+          source={icons.no_internet}
+          resizeMethod="resize"
+          style={styles.icon}
+        />
         <View style={styles.textContainer}>
           <View style={styles.title}>
-            <MediumText
+            <BoldText
               size={22}
               lineHeight={26}
               color={colors.black}
               textAlign="center"
             >
-              {i18n.t("errorHandling.NoInternetTitle")}
-            </MediumText>
+              {i18n.t("errorHandling.noInternetTitle")}
+            </BoldText>
           </View>
-          <RegularText
-            size={18}
-            lineHeight={21}
-            color={colors.black}
-            textAlign="center"
-          >
-            {i18n.t("errorHandling.noInternetConnectionDescription")}
-          </RegularText>
+          <View style={styles.description}>
+            <RegularText
+              size={18}
+              lineHeight={32}
+              color={colors.black}
+              textAlign="center"
+              letterSpacing={0.2}
+            >
+              {i18n.t("errorHandling.noInternetDescription")}
+            </RegularText>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          {/* <BigButton
-            onPress={pop}
-            buttonLabel={i18n.t("errorHandling.noInternetButtonLabel")}
-          /> */}
-        </View>
+        <ActionButton
+          buttonText={i18n.t("errorHandling.noInternetButtonLabel")}
+          buttonColors={[colors.darkGreen, colors.lime]}
+          buttonTextColor={colors.white}
+          buttonTextSize={18}
+          buttonContainerStyle={styles.buttonContainer}
+          onPress={pop}
+        />
       </View>
     </SafeAreaView>
   );
@@ -58,16 +76,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: paddings._24px,
   },
   title: {
     paddingVertical: paddings._8px,
+  },
+  description: {
+    paddingHorizontal: paddings._16px,
   },
   textContainer: {
     paddingVertical: paddings._32px,
     justifyContent: "space-between",
   },
   buttonContainer: {
-    width: "50%",
+    paddingVertical: 18,
+    borderRadius: 12,
+    width: Dimensions.get("screen").width - 48,
+  },
+  icon: {
+    width: 70,
+    height: 70,
   },
 });

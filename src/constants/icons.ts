49c2ed275c99080e14136a19216e2c8411797eb0
Noam Kurtzer <noamkurtzer@gmail.com>
2023-02-11 15:51:26 +0200

    registered avatar icons

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -25,6 +25,43 @@ export const icons: iconsInterface = {
   no_internet: require("../assets/icons/no-signal.png"),
   sad_face: require("../assets/icons/sad-face.png"),
   broken_image: require("../assets/icons/broken-image.png"),
+
+  // Avatars
+  femaleSign: require("../assets/icons/female.png"),
+  maleSign: require("../assets/icons/male.png"),
+  undefinedSign: require("../assets/icons/undefined.png"),
+  // females
+  woman_bangs: require("../assets/icons/avatars/woman_bangs.png"),
+  woman_black_flower: require("../assets/icons/avatars/woman_black_flower.png"),
+  woman_ginger_ponytail: require("../assets/icons/avatars/woman_ginger_ponytail.png"),
+  woman_headphones: require("../assets/icons/avatars/woman_headphones.png"),
+  woman_blond: require("../assets/icons/avatars/woman_straight_blond2.png"),
+  woman_blond2: require("../assets/icons/avatars/woman_straight_blond.png"),
+  woman_brown: require("../assets/icons/avatars/woman_straight_brown.png"),
+  woman_arab: require("../assets/icons/avatars/woman_arab.png"),
+  woman_top: require("../assets/icons/avatars/woman_cleavage.png"),
+  woman_punk: require("../assets/icons/avatars/woman_punk.png"),
+  // males
+  man_arab: require("../assets/icons/avatars/man_arab.png"),
+  man_bold: require("../assets/icons/avatars/man_bold.png"),
+  man_black: require("../assets/icons/avatars/man_black.png"),
+  man_tattoos: require("../assets/icons/avatars/man_tattoos.png"),
+  man_mustache: require("../assets/icons/avatars/man_mustache.png"),
+  man_blond: require("../assets/icons/avatars/man_blond.png"),
+  man_curly: require("../assets/icons/avatars/man_curly.png"),
+  man_prehistoric: require("../assets/icons/avatars/man_prehistoric.png"),
+  man_hoodie: require("../assets/icons/avatars/man_hoodie.png"),
+  man_toxido: require("../assets/icons/avatars/man_toxido.png"),
+  // animals
+  walrus: require("../assets/icons/avatars/walrus.png"),
+  boar: require("../assets/icons/avatars/wild-boar.png"),
+  lion: require("../assets/icons/avatars/wild-boar.png"),
+  snake: require("../assets/icons/avatars/snake.png"),
+  hen: require("../assets/icons/avatars/hen.png"),
+  bat: require("../assets/icons/avatars/bat.png"),
+  cat: require("../assets/icons/avatars/cat.png"),
+  sloth: require("../assets/icons/avatars/sloth.png"),
+  rabbit: require("../assets/icons/avatars/rabbit.png"),
 };
 
 interface iconsInterface {
@@ -47,7 +84,46 @@ interface iconsInterface {
   no_results: ImageSourcePropType;
   abstract_shape1: ImageSourcePropType;
   user_logout: ImageSourcePropType;
+
+  // Error icons
   no_internet: ImageSourcePropType;
   sad_face: ImageSourcePropType;
   broken_image: ImageSourcePropType;
+
+  // Avatars
+  femaleSign: ImageSourcePropType;
+  maleSign: ImageSourcePropType;
+  undefinedSign: ImageSourcePropType;
+  // females
+  woman_bangs: ImageSourcePropType;
+  woman_black_flower: ImageSourcePropType;
+  woman_ginger_ponytail: ImageSourcePropType;
+  woman_headphones: ImageSourcePropType;
+  woman_blond: ImageSourcePropType;
+  woman_blond2: ImageSourcePropType;
+  woman_brown: ImageSourcePropType;
+  woman_arab: ImageSourcePropType;
+  woman_top: ImageSourcePropType;
+  woman_punk: ImageSourcePropType;
+  // males
+  man_arab: ImageSourcePropType;
+  man_bold: ImageSourcePropType;
+  man_black: ImageSourcePropType;
+  man_tattoos: ImageSourcePropType;
+  man_mustache: ImageSourcePropType;
+  man_blond: ImageSourcePropType;
+  man_curly: ImageSourcePropType;
+  man_prehistoric: ImageSourcePropType;
+  man_hoodie: ImageSourcePropType;
+  man_toxido: ImageSourcePropType;
+  // animals
+  walrus: ImageSourcePropType;
+  boar: ImageSourcePropType;
+  lion: ImageSourcePropType;
+  snake: ImageSourcePropType;
+  hen: ImageSourcePropType;
+  bat: ImageSourcePropType;
+  cat: ImageSourcePropType;
+  sloth: ImageSourcePropType;
+  rabbit: ImageSourcePropType;
 }

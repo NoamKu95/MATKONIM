c9a83fd48cb2fd49d173c989e86792bb054e33d5
Noam Kurtzer <noamkurtzer@gmail.com>
2023-02-15 18:58:49 +0200

    Finished ImageSelectionSection

--- src/features/addRecipe/components/ImageSelectionFormPart.tsx
+++ src/features/addRecipe/components/ImageSelectionFormPart.tsx
@@ -6,6 +6,7 @@ import i18n from "../../../translations/i18n";
 // Inner imports:
 import { colors } from "../../../constants/colors";
 import { icons } from "../../../constants/icons";
+import { paddings } from "../../../constants/paddings";
 
 // Redux:
 import { useAppDispatch, useAppSelector } from "../../../store/store";
@@ -30,49 +31,50 @@ const ImageSelectionFormPart = ({
   );
 
   return (
-    <View style={styles.gallerySectionContainer}>
+    <View style={styles().gallerySectionContainer}>
       {renderTitlesOfSection(
         i18n.t("addRecipe.recipeImageTitle"),
         i18n.t("addRecipe.recipeImageSubTitle")
       )}
       <Pressable
-        style={[
-          styles.galleryIconWrapper,
-          {
-            borderColor:
-              imageUri !== null ? colors.darkGreen : colors.transparentBlack5,
-          },
-        ]}
+        style={styles(imageUri !== null).galleryIconWrapper}
         onPress={() => dispatch(openDeviceGallery())}
       >
         <Image
           source={imageUri !== null ? icons.checkmarkGreen : icons.gallery}
           resizeMethod={"resize"}
-          style={[styles.galleryIcon, { opacity: imageUri !== null ? 1 : 0.3 }]}
+          style={styles(imageUri !== null).galleryIcon}
         />
       </Pressable>
-      {renderWarningTextPlaceholder(recipeImageWarning, "center")}
+      <View style={styles().warningContainer}>
+        {renderWarningTextPlaceholder(recipeImageWarning, "center")}
+      </View>
     </View>
   );
 };
 
 export default ImageSelectionFormPart;
 
-const styles = StyleSheet.create({
-  gallerySectionContainer: {
-    paddingTop: 12,
-  },
-  galleryIconWrapper: {
-    alignSelf: "center",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderRadius: 50,
-    padding: 15,
-    marginTop: 8,
-    marginBottom: 12,
-  },
-  galleryIcon: {
-    width: 45,
-    height: 45,
-  },
-});
+const styles = (imageSelected?: boolean) =>
+  StyleSheet.create({
+    gallerySectionContainer: {
+      paddingTop: paddings._12px,
+    },
+    galleryIconWrapper: {
+      alignSelf: "center",
+      borderWidth: 1,
+      borderStyle: "dashed",
+      borderRadius: 50,
+      padding: paddings._16px,
+      marginTop: 8,
+      borderColor: imageSelected ? colors.darkGreen : colors.transparentBlack5,
+    },
+    galleryIcon: {
+      width: 45,
+      height: 45,
+      opacity: imageSelected ? 1 : 0.3,
+    },
+    warningContainer: {
+      paddingTop: paddings._12px,
+    },
+  });
